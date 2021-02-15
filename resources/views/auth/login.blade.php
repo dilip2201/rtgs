697ec2c37b4893a6a766d9ad96355ba58f6e61a2
VikasJKatariya <vikaskatariya16081998@gmail.com>
2021-02-16 00:04:54 +0530

    --login changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,7 +62,7 @@
                            @csrf
                            <div class="form-group py-3 m-0">
                               <label style="color: #b5b5c3!important">Your Email</label>
-                              <input id="email" class="{{ $errors->has('email') ? ' is-invalid' : '' }} form-control h-auto border-0 px-0 placeholder-dark-75" style="    border-bottom: 1px solid #ccc!important;" type="Email" placeholder="Email" name="email" autocomplete="off"  required="" />
+                              <input id="email" class="{{ $errors->has('email') ? ' is-invalid' : '' }} form-control h-auto border-0 px-0 placeholder-dark-75" style="    border-bottom: 1px solid #ccc!important;" type="Email" value="{{ old('email') }}" placeholder="Email" name="email" autocomplete="off"  required="" />
                               @if ($errors->has('email'))
                               <span class="invalid-feedback">
                               <strong>{{ $errors->first('email') }}</strong>
@@ -72,7 +72,7 @@
                            <div class="form-group py-3  m-0" style="position: relative;">
                              
                               <label style="color: #b5b5c3!important">Enter Your Password</label>
-                              <input id="pass_log_id" style="    border-bottom: 1px solid #ccc!important;" type="password" class="{{ $errors->has('password') ? ' is-invalid' : '' }} form-control h-auto border-0 px-0 placeholder-dark-75" name="password" placeholder="Password" required="">
+                              <input id="pass_log_id" style="    border-bottom: 1px solid #ccc!important;" type="password" class="{{ $errors->has('password') ? ' is-invalid' : '' }} form-control h-auto border-0 px-0 placeholder-dark-75" value="{{old('password')}}" name="password" placeholder="Password" required="">
                               <span toggle="#password-field" style="position: absolute; cursor: pointer; top: 46px;    right: 15px;" class="fa fa-fw fa-eye field_icon toggle-password"></span>
                               <input id="password-confirm" type="password" placeholder="Confirm Password"
                                  name="asaspassword_confirmation" autocomplete="new-password" style="display: none;">
